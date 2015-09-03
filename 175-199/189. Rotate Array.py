233dab7688f1276903ba728b181673c11481c0ef
Swapnil Agarwal <swapnilagarwal@live.com>
2015-09-03 20:43:19 +0530

    Add problem 189

---
+++ 175-199/189. Rotate Array.py
@@ -0,0 +1,15 @@
+'''
+Rotate an array of n elements to the right by k steps.
+
+For example, with n = 7 and k = 3, the array [1,2,3,4,5,6,7] is rotated to [5,6,7,1,2,3,4].
+'''
+
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        a=len(nums)-k
+        nums[:]=nums[a:]+nums[:a]
