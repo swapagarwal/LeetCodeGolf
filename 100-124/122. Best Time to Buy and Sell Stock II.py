0c860f33f63f7b6325d347790cb05bf18a734c06
Swapnil Agarwal <swapnilagarwal@live.com>
2015-09-12 22:50:47 +0530

    Update problem 122

--- 100-124/122. Best Time to Buy and Sell Stock II.py
+++ 100-124/122. Best Time to Buy and Sell Stock II.py
@@ -9,5 +9,5 @@ class Solution:
     # @return {integer}
     def maxProfit(self, prices):
         a=0
-        for i in xrange(1,len(prices)):a+=max(prices[i]-prices[i-1],0)
+        for i in range(1,len(prices)):a+=max(prices[i]-prices[i-1],0)
         return a
